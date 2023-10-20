6eb9b5b9a891aa24219259fceb08faf5e2b4bc07
ManuXD <126265298+ManuSam001@users.noreply.github.com>
2023-10-20 11:01:23 -0300

    som

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-document.querySelector('.tecla_som_XD').play();
+//declaração/criação de função tocaSomXD
+function tocaSomXD (){
+    document.querySelector('#som_tecla_XD').play();
+}
+//chamada ou inovação da função TocaSomXD ao clicar no botão
+document.querySelector('.tecla_pom').onclick = tocaSomXD;
+
+//Declaração/criação da função TocaSomAAAUGH
+function TocaSomAAAUGH(){
+    document.querySelector('#som_tecla_AAAUGH').play();
+}
+
+//chamada ou inovação da funçaõ TocaSomAAAUGH ao clicar no botão
+document.querySelector('.tecla_AAAUGH').onclick = TocaSomAAAUGH;
+
