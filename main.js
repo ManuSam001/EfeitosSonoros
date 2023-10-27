169ef585b0c97023d69a9c427183da7245e3ba33
ManuXD <126265298+ManuSam001@users.noreply.github.com>
2023-10-27 10:49:08 -0300

    img

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 //Declaração/criação de função tocaSomXD
-function tocaSomXD (){
-    document.querySelector('#som_tecla_XD').play();
+function tocaSomXD (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 //criando a referências contantes listaDeTeclas e buscando a classe coletiva.tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador =0;
-while(contador < 9){
-    listaDeTeclas[contador].onclick = tocaSomXD;
+while(contador < listaDeTeclas.length){
+    const
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom(idAudio);
+    }
     contador = contador + 1;
     console.log(contador);
 }
