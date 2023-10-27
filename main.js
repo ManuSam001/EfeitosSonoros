c1dc2051f1e2015c9d45e6ca6cbeef47a2977936
ManuXD <126265298+ManuSam001@users.noreply.github.com>
2023-10-27 10:24:12 -0300

    .

--- main.js
+++ main.js
@@ -2,38 +2,10 @@
 function tocaSomXD (){
     document.querySelector('#som_tecla_XD').play();
 }
-//chamada ou inovação da função TocaSomXD ao clicar no botão
-document.querySelector('.tecla_XD').onclick = tocaSomXD;
+//criando a referências contantes listaDeTeclas e buscando a classe coletiva.tecla
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//Declaração/criação da função TocaSomAAAUGH
-function TocaSomAAAUGH (){
-    document.querySelector('#som_tecla_AAAUGH').play();
-}
-//chamada ou inovação da funçaõ TocaSomAAAUGH ao clicar no botão
-document.querySelector('.tecla_AAAUGH').onclick = TocaSomAAAUGH;
-
-//Declaração/criação de função TocaSomMAGAH
-function TocaSomOMAGAH (){
-    document.querySelector('#som_tecla_OMAGAH').play();
-}
-//chamada ou inovação da função TocaSomOMAGAH ao clicar no botão
-document.querySelector('.tecla_OMAGAH').onclick = TocaSomOMAGAH;
-
-//Declaração/criação de função TocaSomBRUH
-function TocaSomBRUH (){
-    document.querySelector('#som_tleca_BRUH').play();
-}
-//chamada ou inovação da funçaõ TocaSomBRUH ao clicar no botão
-document.querySelector('.tecla_BRUH').onclick = TocaSomBRUH;
-
-//Declaração/criação da função TocaSom!!
-function TocaSom!! (){
-    document.querySelector('#som_tecla_!!').play
-}
-//chamada ou inovação da função TocaSom!! ao clicar no botão
-document.querySelector('.tecla_!!').onclick = TocaSom!!
-
-//Declaração/criação de função TocaSomCAVALO
-function TocaSomCAVALO (){
-    document.querySelector('')
+let contador =0;
+while(contador < 9){
+    listaDeTeclas[contador].onclick = tocaSomXD;
 }
