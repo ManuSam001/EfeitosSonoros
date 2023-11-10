35b3f9a19ce191e645412a13fd12f42cfcdb3bd2
ManuXD <126265298+ManuSam001@users.noreply.github.com>
2023-11-10 11:14:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ function tocaSomXD (idElementoAudio) {
 //criando a referências contantes listaDeTeclas e buscando a classe coletiva.tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
 //criaçõa da repetição "while" e trocado pelo "for" com variavel,condição e incremento
-for(let contador = 0; contador < listaDeTeclas.length); contador++){
-    const tecla = listaDeTeclas [contador]; //guarda o contador da lista
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const teclas = listaDeTeclas [contador]; //guarda o contador da lista
     const efeito = teclas.classList[1];   //chama o 2° item classe individual do som
-    const idAudio = `#som_${efeito};    //utiliza a junção template string  ${ }
+    const idAudio = `#som_${efeito}`;    //utiliza a junção template string 
     teclas.onclick = function(){       //função anônima auxiliar para chamar tocaSom
-        tocaSom(idAudio);         //chamada da função tocaSom com parâmetro idAudio
+        tocaSomXD(idAudio);         //chamada da função tocaSom com parâmetro idAudio
     }
+}
